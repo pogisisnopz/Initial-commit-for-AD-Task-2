aebd9c6637738601c57dfc3ac1f584435fc4627f
Van Ryan M. Navarez <202310560@fit.edu.ph>
2025-05-23 23:13:00 +0800

    Create details.php

---
+++ details.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+
+echo "<h2>Product Details</h2>";
+echo "<p>This is a static detail page. You can describe any product here as a sample.</p>";
+
+include 'footer.php';
+?>
