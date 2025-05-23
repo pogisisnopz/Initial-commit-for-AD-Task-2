3ca203ab5374f3b60ec2cbcd1be3734ffdf071e3
Van Ryan M. Navarez <202310560@fit.edu.ph>
2025-05-23 23:08:44 +0800

    data.php

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+// Array of associative arrays (dictionary-style)
+$products = [
+    ["id" => 1, "name" => "Laptop", "price" => 800],
+    ["id" => 2, "name" => "Headphones", "price" => 150],
+    ["id" => 3, "name" => "Keyboard", "price" => 100]
+];
+?>
